c3cdd4b0182965e4988812cab0a673482b5cec6c
Hildor Junior <hildorjunior@gmail.com>
2020-09-11 08:55:43 -0300

    feat: home rework

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
-    <section class="hero is-fullheight is-primary">
+    <section class="hero is-fullheight">
       <div class="hero-body">
         <div class="container">
           <div class="columns">
             <div class="column is-6 has-text-centered">
-              <h1 class="title has-text-black">unze</h1>
-              <p class="subtitle has-text-black">Consultoria de TI</p>
+              <p class="subtitle">Agência Digital</p>
+              <h1 class="title">unze</h1>
               <br>
-              <p class="has-text-black">
-                Entre em contato
-                pelo e-mail <a href="mailto:hildorjunior@gmail.com">hildorjunior@gmail.com</a>
+              <p>
+                Entre em contato e faça sua empresa decolar:
+                 <a href="mailto:hildorjunior@gmail.com">hildorjunior@gmail.com</a>
               </p>
             </div>
           </div>
@@ -27,16 +27,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/variables.scss';
 .hero {
-  background-image: url('../assets/i-2.svg');
-  background-repeat: no-repeat;
-  background-size: 40%;
-  background-position: 90%;
-  @media screen and (max-width: 768px) {
-    background-image: none;
+  background-image: url('../assets/rocket-bg-2.jpg');
+  background-size: cover;
+  background-position-y: 95%;
+  .title {
+    font-size: 8em;
+    color: $primary;
+    text-shadow: 3px 3px 0 $link;
   }
-}
-.title {
-  font-size: 8em;
+  .subtitle {
+    color: hsl(215, 22%, 95%);
+  }
+  p, a {
+    color: hsl(215, 22%, 95%);
+  }
 }
 </style>
